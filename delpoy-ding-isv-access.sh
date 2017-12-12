dcdacb63c0277bd024e52b102d1f46c2d8afbac9
lifeng.zlf <zhonglifeng@aliyun.com>
2017-12-12 14:07:38 +0800

    Update delpoy-ding-isv-access.sh
    start

--- delpoy-ding-isv-access.sh
+++ delpoy-ding-isv-access.sh
@@ -4,4 +4,4 @@ kill -9 `ps -ef | grep tomcat | grep -v grep | awk '{print $2}'`
 rm -rf /usr/local/soft/apache-tomcat-7.0.67/webapps/ding-isv-accesss.war
 rm -rf /usr/local/soft/apache-tomcat-7.0.67/webapps/ding-isv-accesss
 cp  /home/mint/workspace/ding-isv-access/web/target/ding-isv-accesss.war /usr/local/soft/apache-tomcat-7.0.67/webapps/
-#sh /usr/local/soft/apache-tomcat-7.0.67/bin/startup.sh
+sh /usr/local/soft/apache-tomcat-7.0.67/bin/startup.sh
